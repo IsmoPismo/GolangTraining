e35374e7d31fe9762342d8cab82a6653a17379e4
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-15 16:32:08 +0100

    Packages

---
+++ 03_Package/ismarpackage/name.go
@@ -0,0 +1,4 @@
+package ismarpackage
+
+// MyName will be exported because it starts with a capital letter.
+var MyName = "Ismar"
---
+++ 03_Package/main/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/IsmoPismo/GolangTraining/03_Package/ismarpackage"
+)
+
+func main() {
+	fmt.Println(ismarpackage.Reverse("!oG ,olleH"))
+	fmt.Println(ismarpackage.MyName)
+}
