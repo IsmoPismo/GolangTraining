e190891d1e362704a7a5eabaa1c6e8804c10239c
Ismar Sacirovic <ismarguitar@gmail.com>
2018-01-28 19:42:14 +0100

    Creating a new Deck and printing this Deck

---
+++ 99_Deck_of_Cards/Cards/deck.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	cardNum := []string{"Ace of ", "Deuce of ", "Three of ", "Four of ", "Five of ", "Six of ", "Seven of ", "Eigth of ", "Nine of", "Ten of ", "Jack of ", "Queen of ", "King of "}
+	cardSign := []string{"Hearts", "Diamonts", "Clubs", "Spades"}
+
+	for _, sign := range cardSign {
+		for _, num := range cardNum {
+			cards = append(cards, num+sign)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
--- 99_Deck_of_Cards/main.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-	cards := deck{"Ace of Spades", "Deuce of Clubs", "King of Diamonts", "Queen of Hearts"}
-	cards.print()
-}
