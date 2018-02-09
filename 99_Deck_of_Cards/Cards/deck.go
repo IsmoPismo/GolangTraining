15fe1bd2ba8a18b58065b0ccf3450824eb8410de
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-09 19:10:27 +0100

    Not quite random

--- 99_Deck_of_Cards/Cards/deck.go
+++ 99_Deck_of_Cards/Cards/deck.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
 	"strings"
 )
@@ -47,4 +48,11 @@ func newDeckFromFile(filename string) deck {
 		os.Exit(1)
 	}
 	return deck(strings.Split(string(bs), ","))
+}
+
+func (d deck) shuffle() {
+	for i := range d {
+		np := rand.Intn(len(d) - 1)
+		d[i], d[np] = d[np], d[i]
+	}
 }
--- 99_Deck_of_Cards/Cards/main.go
+++ 99_Deck_of_Cards/Cards/main.go
@@ -2,8 +2,6 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, _ := deal(cards, 2)
-	hand.saveToFile("Ace and Deuce")
-	file := newDeckFromFile("Ace and Deuce")
-	file.print()
+	cards.shuffle()
+	cards.print()
 }
