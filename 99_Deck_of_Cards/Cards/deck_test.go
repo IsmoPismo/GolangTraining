3fe7ca74e52dc9ce3970b4ef5895a6d40a65dea5
Ismar Sacirovic <ismarguitar@gmail.com>
2018-02-11 18:57:56 +0100

    2 more tests

--- 99_Deck_of_Cards/Cards/deck_test.go
+++ 99_Deck_of_Cards/Cards/deck_test.go
@@ -8,4 +8,12 @@ func TestNewDeck(t *testing.T) {
 	if len(d) != 52 {
 		t.Errorf("Expected deck is 52 but got %v cards", len(d))
 	}
+
+	if d[0] != "Ace of Hearts" {
+		t.Errorf("Expected first card is not %v but Ace of Hearts", d[0])
+	}
+
+	if d[len(d)-1] != "King of Spades" {
+		t.Errorf("Expected last card is no %v but the King of Spades", d[len(d)-1])
+	}
 }
